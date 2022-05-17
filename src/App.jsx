5d882b7550bef01b3fd8960cef2c20b8089387ac
Julia <79936455+juliajespersdotter@users.noreply.github.com>
2022-05-17 11:47:04 +0200

    scss skeleton files.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
-import { useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom'
-import socketio from 'socket.io-client'
-import BattleboardPage from './pages/BattleboardPage';
-import NotFound from './pages/NotFound'
-import Login from './pages/Login';
-import './App.css';
-
-// const socket = socketio.connect(process.env.REACT_APP_SOCKET_URL)
+import { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import socketio from "socket.io-client";
+import BattleboardPage from "./pages/BattleboardPage";
+import NotFound from "./pages/NotFound";
+import Login from "./pages/Login";
+import "./App.css";
 
 function App() {
-  return (
-    <div id="App">
+	return (
+		<div id="App">
 			<Routes>
 				<Route path="/" element={<Login />} />
 				<Route path="/games/:game_id" element={<BattleboardPage />} />
 				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</div>
-  );
+	);
 }
 
 export default App;
