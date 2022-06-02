4a9324efda5880d9f86fbc16c6cfb4ef6b148a1c
Julia Jespersdotter <julia.jespersdotter@gmail.com>
2022-06-02 12:10:45 +0200

    cleaning up code and removing comments.

--- src/pages/BattleboardPage.jsx
+++ src/pages/BattleboardPage.jsx
@@ -2,15 +2,12 @@ import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useGameContext } from "../contexts/GameContextProvider";
 import Battleboard from "../components/Battleboard";
-// import Loser from "../components/Loser"
 import WaitingRoom from "./WaitingRoom";
 import Logo from "../assets/images/logo.png";
 import "normalize.css";
 import "../assets/css/BattleboardPage.css";
 
 const BattleboardPage = () => {
-	// const [players, setPlayers] = useState([]);
-	// const [connected, setConnected] = useState(false);
 	const [disconnectedMsg, setDisconnectedMsg] = useState(false);
 	const [disconnected, setDisconnected] = useState("");
 	const [waiting, setWaiting] = useState(true);
@@ -20,30 +17,32 @@ const BattleboardPage = () => {
 	const navigate = useNavigate();
 	const [turn, setTurn] = useState();
 
-	const handleUpdatePlayers = (playerlist) => {
-		// console.log("Got new playerlist", playerlist);
-		// setPlayers(playerlist);
-		setTurn(Object.values(playerlist)[0]);
-
-		if (Object.keys(playerlist).length === 2) {
-			if (Object.values(playerlist)[0] === gameUsername) {
-				setEnemy(Object.values(playerlist)[1]);
-			} else {
-				setEnemy(Object.values(playerlist)[0]);
-			}
-
-			setWaiting(false);
-			setDisconnectedMsg(false);
-
-			socket.emit("update-list");
-		} else if (Object.keys(playerlist).length === 1) {
-			setWaiting(true);
-			socket.emit("update-list");
-		}
-	};
-
 	// connect to room when component is mounted
 	useEffect(() => {
+		const handleUpdatePlayers = (playerlist) => {
+			// set turn to be the first player in player list
+			setTurn(Object.values(playerlist)[0]);
+
+			if (Object.keys(playerlist).length === 2) {
+				if (Object.values(playerlist)[0] === gameUsername) {
+					setEnemy(Object.values(playerlist)[1]);
+				} else {
+					setEnemy(Object.values(playerlist)[0]);
+				}
+
+				setWaiting(false);
+				setDisconnectedMsg(false);
+
+				// update list of games on server to update Login page that this room is closed
+				socket.emit("update-list");
+
+				// if player leaves update games on server that this room is now open
+			} else if (Object.keys(playerlist).length === 1) {
+				setWaiting(true);
+				socket.emit("update-list");
+			}
+		};
+
 		// if no username, redirect them to the login page
 		if (!gameUsername) {
 			navigate("/");
@@ -52,22 +51,16 @@ const BattleboardPage = () => {
 
 		// emit join request
 		socket.emit("player:joined", gameUsername, game_id, (status) => {
-			// console.log(
-			// 	`Successfully joined ${game_id} as ${gameUsername}`,
-			// 	status
-			// );
-			// setConnected(true);
+			console.log(
+				`Successfully joined ${game_id} as ${gameUsername}`,
+				status
+			);
 		});
 
 		// listen for updated userlist
-
 		socket.on("player:list", handleUpdatePlayers);
 
 		return () => {
-			// console.log("Running cleanup");
-
-			// socket.off("player:list", handleUpdatePlayers);
-
 			socket.on("player:disconnected", (username) => {
 				setDisconnected(username);
 				setDisconnectedMsg(true);
@@ -76,8 +69,8 @@ const BattleboardPage = () => {
 			// disconnect player
 			socket.emit("player:left", gameUsername, game_id);
 		};
-		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [socket, game_id, gameUsername, navigate]);
+
 	return (
 		<div className="gamewrapper">
 			<div className="game-header">
@@ -96,33 +89,11 @@ const BattleboardPage = () => {
 			{waiting && (
 				<div>
 					<WaitingRoom />
-					{/* <Loser /> */}
 				</div>
 			)}
 
-			{/* {countdown && (
-					<div className="countdown-timer">
-						<img src={Count} alt="" />
-					</div>
-			)} */}
-			{/* <h1 className="game-tagline">Let's Battleship</h1>
-				{waiting && 
-				<p>Waiting for player...</p>}
-				<div id="players">
-				<h1 className="game-tagline">Let's Battleship</h1>
-				{waiting && <p>Waiting for player...</p>}
-				{/* <div id="players">
-					<h2>Players</h2>
-					<ul className="online-players">
-						
-					<li>You: {gameUsername}</li>
-					<li>Enemy: {enemy}</li>
-					</ul>
-				</div> */}
-
 			{!waiting && (
 				<>
-					{/* <p>Game is starting!</p> */}
 					<Battleboard
 						yourName={gameUsername}
 						enemy={enemy}
