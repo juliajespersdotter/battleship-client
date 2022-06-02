2b57f7f66604e3d84a27aa275a73f58c6c854f7d
Julia Jespersdotter <julia.jespersdotter@gmail.com>
2022-06-02 13:17:16 +0200

    added chat function.

--- src/pages/BattleboardPage.jsx
+++ src/pages/BattleboardPage.jsx
@@ -1,4 +1,8 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import ListGroup from "react-bootstrap/ListGroup";
 import { useNavigate, useParams } from "react-router-dom";
 import { useGameContext } from "../contexts/GameContextProvider";
 import Battleboard from "../components/Battleboard";
@@ -8,6 +12,12 @@ import "normalize.css";
 import "../assets/css/BattleboardPage.css";
 
 const BattleboardPage = () => {
+	// chat code
+	const [message, setMessage] = useState("");
+	const [messages, setMessages] = useState([]);
+	const messageRef = useRef();
+
+	// game logic code
 	const [disconnectedMsg, setDisconnectedMsg] = useState(false);
 	const [disconnected, setDisconnected] = useState("");
 	const [waiting, setWaiting] = useState(true);
@@ -16,6 +26,42 @@ const BattleboardPage = () => {
 	const { game_id } = useParams();
 	const navigate = useNavigate();
 	const [turn, setTurn] = useState();
+
+	const handleIncomingMessage = (msg) => {
+		console.log("Received a new chat message", msg);
+
+		// add message to chat
+		setMessages((prevMessages) => [...prevMessages, msg]);
+	};
+
+	const handleSendMessage = async (e) => {
+		e.preventDefault();
+
+		if (!message.length) {
+			return;
+		}
+
+		// construct message object
+		const msg = {
+			username: gameUsername,
+			game: game_id,
+			content: message,
+			timestamp: Date.now(),
+		};
+
+		// emit chat message
+		socket.emit("chat:message", msg);
+
+		// add message to chat
+		setMessages((prevMessages) => [
+			...prevMessages,
+			{ ...msg, self: true },
+		]);
+
+		// clear message input and refocus on input element
+		setMessage("");
+		messageRef.current.focus();
+	};
 
 	// connect to room when component is mounted
 	useEffect(() => {
@@ -57,6 +103,9 @@ const BattleboardPage = () => {
 			);
 		});
 
+		// listen for incoming messages
+		socket.on("chat:message", handleIncomingMessage);
+
 		// listen for updated userlist
 		socket.on("player:list", handleUpdatePlayers);
 
@@ -66,10 +115,18 @@ const BattleboardPage = () => {
 				setDisconnectedMsg(true);
 			});
 
+			// stop listening to events
+			socket.off("chat:message", handleIncomingMessage);
+
 			// disconnect player
 			socket.emit("player:left", gameUsername, game_id);
 		};
 	}, [socket, game_id, gameUsername, navigate]);
+
+	useEffect(() => {
+		// focus on message input
+		messageRef.current && messageRef.current.focus();
+	}, []);
 
 	return (
 		<div className="gamewrapper">
@@ -101,6 +158,50 @@ const BattleboardPage = () => {
 					/>
 				</>
 			)}
+
+			<div id="chat">
+				<h2>#{game_id}</h2>
+
+				<div id="messages-wrapper">
+					<ListGroup id="messages">
+						{messages.map((message, index) => {
+							const ts = new Date(message.timestamp);
+							const time = ts.toLocaleTimeString();
+							return (
+								<ListGroup.Item key={index} className="message">
+									<span className="time">{time}</span>
+									<span className="user">
+										{message.username}:
+									</span>
+									<span className="content">
+										{message.content}
+									</span>
+								</ListGroup.Item>
+							);
+						})}
+					</ListGroup>
+				</div>
+
+				<Form onSubmit={handleSendMessage} id="message-form">
+					<InputGroup>
+						<Form.Control
+							onChange={(e) => setMessage(e.target.value)}
+							placeholder="Say something nice..."
+							ref={messageRef}
+							required
+							type="text"
+							value={message}
+						/>
+						<Button
+							variant="success"
+							type="submit"
+							disabled={!message.length}
+						>
+							Send
+						</Button>
+					</InputGroup>
+				</Form>
+			</div>
 		</div>
 	);
 };
